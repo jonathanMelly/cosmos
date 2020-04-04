1dea18b12cd33ea371765aa74994c7edd847ab14
Jonathan Melly <jonathan.melly@gmail.com>
2020-04-04 17:22:06 +0200

    fixes #16 Partie de code déplacée dans le pipeline

--- src/test/TestInterpreter.cs
+++ src/test/TestInterpreter.cs
@@ -55,46 +55,6 @@ namespace test
             //Assert
             Assert.Matches(ValidExecutionContent,fakeConsole.ToString());
         }
-        
-        [Fact (Skip = "Better done directly on pipeline using process exit code")]
-        //Inspired from https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.process.outputdatareceived?view=netcore-3.1
-        public void IntegrationTest()
-        {
-        
-            //Arrange
-            var output = new StringBuilder();
-            
-            Process process = new Process();
-            process.StartInfo.FileName = "dotnet";
-            process.StartInfo.Arguments="run --project ../../../../interpreter/interpreter.csproj "+ValidProgramFile;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = true;
-            
-            var dreh = new DataReceivedEventHandler((sender, e) =>
-            {
-                // Prepend line numbers to each line of the output.
-                if (!String.IsNullOrEmpty(e.Data))
-                {
-                    output.Append(e.Data);
-                }
-            });
-            process.OutputDataReceived += dreh;
-            process.ErrorDataReceived += dreh;
-            
-            //Act
-            process.Start();
 
-            // Asynchronously read the standard output of the spawned process. 
-            // This raises OutputDataReceived events for each line of output.
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-            process.WaitForExit(10000);
-            process.Close();
-
-            //Assert
-            output.ToString().Should().Match(ValidExecutionContent);
-        }
-        
     }
 }
