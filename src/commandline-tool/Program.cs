2b6d86df9a316b9c66cd61cf882c0196d39fa62d
Jonathan Melly <jonathan.melly@gmail.com>
2020-04-21 14:01:51 +0200

    AB#36 added commandline-tool project
    Took 2 minutes

---
+++ src/commandline-tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace commandline_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
