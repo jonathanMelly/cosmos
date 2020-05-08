e8f8ff11209c9179a66b19b7a848337844007c0b
Jonathan Melly <jonathan.melly@gmail.com>
2020-05-08 16:23:21 +0200

    test
    Took 40 minutes

--- src/commandline-tool/Program.cs
+++ src/commandline-tool/Program.cs
@@ -14,7 +14,7 @@ namespace commandline_tool
 
         public static int Main(string[] args)
         {
-            //TODO gérer les options en ligne de commande (compilation, éxécution, ...)
+            //TODO gérer les options en ligne de  commande (compilation, éxécution, ...)
             var parser = new Parser().ForFile(args[0]);
             var interpreter = new Interpreter(parser);
             var result = interpreter.Execute();
